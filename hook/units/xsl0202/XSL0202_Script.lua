e35669d301c5c83e38d4a0952ae85a1c2ad1b1e0
speed2CZ <GTGTGT@seznam.cz>
2017-10-19 12:03:44 +0200

    Constructive hook for T2 chicken

--- hook/units/xsl0202/XSL0202_Script.lua
+++ hook/units/xsl0202/XSL0202_Script.lua
@@ -1,17 +1,7 @@
-#****************************************************************************
-#**
-#**  File     :  /units/XSL0202/XSL0202_script.lua
-#**
-#**  Summary  :  Seraphim Heavy Bot Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
-local SWalkingLandUnit = import('/lua/seraphimunits.lua').SWalkingLandUnit
-local SDFAireauBolterWeapon = import('/lua/seraphimweapons.lua').SDFAireauBolterWeapon02
+----------------------------
+-- Seraphim Heavy Bot Script
+----------------------------
 local SDEffectsTemplate = import('/mods/rks_explosions/lua/SDEffectTemplates.lua')
-local sdexplosion = import('/mods/rks_explosions/lua/SDExplosions.lua')
-local RKEffectUtil = import('/mods/rks_explosions/lua/RKEffectUtilities.lua')
-local BoomSoundBP = import('/mods/rks_explosions/boomsounds/BoomSounds.bp')
 local RKExplosion = import('/mods/rks_explosions/lua/SDExplosions.lua')
 
 function TableHas(table, string)
@@ -21,20 +11,18 @@ function TableHas(table, string)
     return false
 end
 
-XSL0202 = Class(SWalkingLandUnit) {
-    Weapons = {
-        MainGun = Class(SDFAireauBolterWeapon) {}
-    },
-	CreateEffects = function( self, EffectTable, army, scale)
+local oldXSL0202 = XSL0202
+XSL0202 = Class(oldXSL0202) {
+    CreateEffects = function( self, EffectTable, army, scale)
         for k, v in EffectTable do
-		if self.RKEmitters == nil then self.RKEmitters = {} end
-			local emitter = CreateAttachedEmitter(self, -1, army, v):ScaleEmitter(scale)
+        if self.RKEmitters == nil then self.RKEmitters = {} end
+            local emitter = CreateAttachedEmitter(self, -1, army, v):ScaleEmitter(scale)
             table.insert(self.RKEmitters, emitter)
-			self.Trash:Add(emitter)
+            self.Trash:Add(emitter)
         end
     end,
-	
-	CreateFluffyHeartShockwave = function( self )
+    
+    CreateFluffyHeartShockwave = function( self )
         local blanketSides = 18/2
         local blanketAngle = (2*math.pi) / blanketSides
         local blanketStrength = 1/2
@@ -43,27 +31,25 @@ XSL0202 = Class(SWalkingLandUnit) {
         for i = 0, (blanketSides-1) do
             local blanketX = math.sin(i*blanketAngle)
             local blanketZ = math.cos(i*blanketAngle)
-			local BlanketOffset = 0
-			local Deceleration = 0 ##-17/2*3*3
-			local Blanketparts = self:CreateProjectile('/mods/rks_explosions/effects/entities/Heart_Debris/Heart_Debris_proj.bp', blanketX, 1.55	+BlanketOffset, blanketZ + 0, blanketX, 0, blanketZ)
+            local BlanketOffset = 0
+            local Deceleration = 0 -- -17/2*3*3
+            local Blanketparts = self:CreateProjectile('/mods/rks_explosions/effects/entities/Heart_Debris/Heart_Debris_proj.bp', blanketX, 1.55 + BlanketOffset, blanketZ + 0, blanketX, 0, blanketZ)
                 :SetVelocity(blanketVelocity):SetAcceleration(Deceleration)
         end        
     end,
-	
-	
-	
+    
     OnKilled = function(self, instigator, type, overkillRatio)
-        SWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio) -- Remove this call if adding everything anyway
-		local bp = self:GetBlueprint()
+        oldXSL0202.OnKilled(self, instigator, type, overkillRatio) -- Remove this call if adding everything anyway
+        local bp = self:GetBlueprint()
         local Army = self:GetArmy()
         local luxyAliases = {"LuXy", "Lu_Xun_17", "Sheeo", "Pinky_Zoria", "RK4000", "TA4Life"} -- Add as many as you want
-		
+        
         if TableHas(luxyAliases, ArmyBrains[self:GetArmy()].Nickname) then
-			self:CreateFluffyHeartShockwave()
+            self:CreateFluffyHeartShockwave()
             self.CreateEffects( self, SDEffectsTemplate.LuXy_Heart, Army, 1)-- Heart explosion goes here
-			##RKExplosion.CreateInheritedVelocityDebrisProjectiles( self, 4, {self:GetVelocity()}, 7.75, 0.23, 50.35, ('/mods/rks_explosions/effects/entities/Heart_Debris/Heart_Debris_proj.bp'))
-            LOG("Heart!")
+            --RKExplosion.CreateInheritedVelocityDebrisProjectiles( self, 4, {self:GetVelocity()}, 7.75, 0.23, 50.35, ('/mods/rks_explosions/effects/entities/Heart_Debris/Heart_Debris_proj.bp'))
         end
     end
 }
+
 TypeClass = XSL0202
