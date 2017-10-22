e030ddff8859a65923674f12ddcb6c087c9bf866
speed2CZ <GTGTGT@seznam.cz>
2017-10-22 11:50:52 +0200

    Update mod_info

--- mod_info.lua
+++ mod_info.lua
@@ -1,14 +1,14 @@
-name = "rks_explosions V9 BETA"
-uid = "EFFFA8C6-96D9-11E4-9DA1-432D1D5D46B0"
-version = 1
-description = "Non-toggle version of V9 beta made playable as per request. :)"
-author = "RK4000, Sheeo, Everyone that posted feedback on the forums."
-url = "FAF forums, check useful mods subthread."
+name = "RKs Explosions"
+uid = "1dd86878-6d2f-4f6d-b879-d14d37bcd45b"
+version = 9
+description = "RKs Explosions mod replaces the default explosions with a new enhanced explosions that are unique for each faction. This is a non-togglable version."
+author = "RK4000, Sheeo, speed2 and everyone that posted feedback on the forums."
+url = "http://forums.faforever.com/viewtopic.php?f=41&t=6813"
 selectable = true
 enabled = true
 exclusive = false
 ui_only = false
-icon = "/mods/rks_explosions/icon.jpg" 
+icon = "/mods/rks_explosions/icon.jpg"
 before = {}
 after = {}
 conflicts = {}
