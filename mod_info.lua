aa569cf2dd0f1ffd435909e1587adff7ca58879c
RK <orvolraz@yahoo.com>
2020-03-15 15:41:58 +0100

    Revert "Revert "Change mod version to V10""
    This reverts commit 73aebb4fef0a5ca7bf3d2787d06224094cdd1eae.

--- mod_info.lua
+++ mod_info.lua
@@ -1,7 +1,7 @@
-name = "RKs Explosions"
-uid = "1dd86878-6d2f-4f6d-b879-d14d37bcd45b"
-version = 9
-description = "RKs Explosions mod replaces the default explosions with a new enhanced explosions that are unique for each faction. This is a non-togglable version."
+name = "RKs Explosions TESTING VERSION"
+uid = "1dd86828-6d2f-4f6d-b879-d14d37bcd45b"
+version = 10
+description = "RKs Explosions mod replaces the default explosions with a new enhanced explosions that are unique for each faction. This is a non-togglable version. TEST VERSION 10."
 author = "RK4000, Sheeo, speed2 and everyone that posted feedback on the forums."
 url = "http://forums.faforever.com/viewtopic.php?f=41&t=6813"
 selectable = true
