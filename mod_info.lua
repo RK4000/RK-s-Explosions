e1bb7cf288022bfce1dd65faa8e9643d261091c3
speed2CZ <GTGTGT@seznam.cz>
2020-03-23 18:51:22 +0100

    Update mod info

--- mod_info.lua
+++ mod_info.lua
@@ -1,7 +1,7 @@
-name = "RKs Explosions TESTING VERSION"
-uid = "1dd86828-6d2f-4f6d-b879-d14d37bcd45b"
+name = "RKs Explosions"
+uid = "90312bd2-6fbd-4aa6-874e-c5057cefc19a"
 version = 10
-description = "RKs Explosions mod replaces the default explosions with a new enhanced explosions that are unique for each faction. This is a non-togglable version. TEST VERSION 10."
+description = "RKs Explosions mod replaces the default explosions with a new enhanced explosions that are unique for each faction. This is a non-togglable version. VERSION 10."
 author = "RK4000, Sheeo, speed2 and everyone that posted feedback on the forums."
 url = "http://forums.faforever.com/viewtopic.php?f=41&t=6813"
 selectable = true
