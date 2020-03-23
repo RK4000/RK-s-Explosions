346446e2edac47125b82281fe464d8adb96226ca
RK <lovro.razpotnik@gmail.com>
2020-03-23 21:06:02 +0100

    Typo

--- mod_info.lua
+++ mod_info.lua
@@ -2,6 +2,7 @@ name = "RK's Explosions"
 uid = "90312bd2-6fbd-4aa6-874e-c5057cefc19a"
 version = 10
 description = "Replaces the default unit explosion effects with factionally varied and higher quality equivalents."
+author = "RK4000, speed2, Sheeo, Domino"
 url = "http://forums.faforever.com/viewtopic.php?f=41&t=6813"
 selectable = true
 enabled = true
